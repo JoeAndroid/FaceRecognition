f7b5cb3554858add4603ff7ae81b9297e6b8efb5
joeAndroid <joeqiaobing@sina.cn>
2018-01-30 14:46:52 +0800

    修改

--- app/src/main/java/com/ughouse/facegverify/MyApplication.java
+++ app/src/main/java/com/ughouse/facegverify/MyApplication.java
@@ -1,21 +1,15 @@
 package com.ughouse.facegverify;
 
-import android.content.Context;
+import android.app.Application;
 import android.net.Uri;
 
-import com.marswin89.marsdaemon.DaemonApplication;
-import com.marswin89.marsdaemon.DaemonConfigurations;
 import com.ughouse.facegverify.constant.FunctionType;
 import com.ughouse.facegverify.db.FaceDB;
-import com.ughouse.facegverify.receiver.AlarmReceiver;
-import com.ughouse.facegverify.receiver.BootReceiver;
-import com.ughouse.facegverify.service.DaemonService;
-import com.ughouse.facegverify.service.FRService;
 
 /**
  * Created by qiaobing on 2018/1/16.
  */
-public class MyApplication extends DaemonApplication {
+public class MyApplication extends Application {
 
     private final String TAG = this.getClass().toString();
     public FaceDB mFaceDB;
@@ -41,42 +35,5 @@ public class MyApplication extends DaemonApplication {
 
     public Uri getCaptureImage() {
         return mImage;
-    }
-
-    /**
-     * give the configuration to lib in this callback
-     *
-     * @return
-     */
-    @Override
-    protected DaemonConfigurations getDaemonConfigurations() {
-        DaemonConfigurations.DaemonConfiguration configuration1 = new DaemonConfigurations.DaemonConfiguration(
-                "com.ughouse.facegverify:process1",
-                FRService.class.getCanonicalName(),
-                BootReceiver.class.getCanonicalName());
-
-        DaemonConfigurations.DaemonConfiguration configuration2 = new DaemonConfigurations.DaemonConfiguration(
-                "com.ughouse.facegverify:process2",
-                DaemonService.class.getCanonicalName(),
-                AlarmReceiver.class.getCanonicalName());
-
-        DaemonConfigurations.DaemonListener listener = new MyDaemonListener();
-        //return new DaemonConfigurations(configuration1, configuration2);//listener can be null
-        return new DaemonConfigurations(configuration1, configuration2, listener);
-    }
-
-
-    class MyDaemonListener implements DaemonConfigurations.DaemonListener {
-        @Override
-        public void onPersistentStart(Context context) {
-        }
-
-        @Override
-        public void onDaemonAssistantStart(Context context) {
-        }
-
-        @Override
-        public void onWatchDaemonDaed() {
-        }
     }
 }
